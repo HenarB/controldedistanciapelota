291de357d13e45db573f07fe448748c7ddf6b015
Henar Brenlla <hbrenlla001@gmail.com>
2019-07-26 14:08:07 +0200

    model

---
+++ Simulink/Sfunction_model/servosfunc/servosfunc_ert_rtw/servosfunc_types.h
@@ -0,0 +1,33 @@
+/*
+ * File: servosfunc_types.h
+ *
+ * Code generated for Simulink model 'servosfunc'.
+ *
+ * Model version                  : 1.50
+ * Simulink Coder version         : 8.14 (R2018a) 06-Feb-2018
+ * C/C++ source code generated on : Fri Jul 26 13:41:42 2019
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_servosfunc_types_h_
+#define RTW_HEADER_servosfunc_types_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+
+/* Parameters (default storage) */
+typedef struct P_servosfunc_T_ P_servosfunc_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_servosfunc_T RT_MODEL_servosfunc_T;
+
+#endif                                 /* RTW_HEADER_servosfunc_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
